4a18c9debfbb49741effc68d3fc052f21f455b3a
Noel <crisherper52@gmail.com>
2022-12-06 20:03:02 -0600

    Spring

---
+++ src/main/java/com/ittg/api/MyFirstProjectSpringApplication.java
@@ -0,0 +1,15 @@
+package com.ittg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MyFirstProjectSpringApplication {
+
+public static void main(String[] args) {
+
+SpringApplication.run(MyFirstProjectSpringApplication.class, args);
+
+}
+}
